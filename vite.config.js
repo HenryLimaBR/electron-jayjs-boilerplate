1241f551df75e4998f6e4da093f05c8fc2042d9a
Henry Lima <djfilipebr@gmail.com>
2024-03-26 02:22:59 -0300

    Update Config

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ export default {
     electron({
       main: {
         // Shortcut of `build.lib.entry`
-        entry: 'electron/main.ts',
+        entry: 'src/main/main.ts',
       },
-      // preload: {
-      //   // Shortcut of `build.rollupOptions.input`
-      //   input: 'electron/preload.ts',
-      // },
-      // // Optional: Use Node.js API in the Renderer process
+      preload: {
+        // Shortcut of `build.rollupOptions.input`
+        input: 'src/preload/preload.ts',
+      },
+      // Optional: Use Node.js API in the Renderer process
       // renderer: {},
     }),
   ],
